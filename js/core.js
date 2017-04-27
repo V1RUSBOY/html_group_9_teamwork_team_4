c1885b0a850d6c2404a77a91a7c5bd32a4764a54
Alexander Kim <alexanderkim3125@gmail.com>
2017-04-27 14:46:11 +0600

    add adaptive header and slider, text for slider is not adaptive

--- js/core.js
+++ js/core.js
@@ -1,14 +1,21 @@
-$(document).ready(function(){
-
-	$('#menu-btn').click(function(){		
-			$('.menu').slideToggle(700);	
-			if ($(this).hasClass('not-active')) {
-				$(this).addClass('is-active').removeClass('not-active');
-			}else{
-				setTimeout(function(){
-					$('.is-active').addClass('not-active').removeClass('is-active')
-				},600)			
-			}		
-		});
-
+$(document).ready(function() {
+	$('.carousel').slick({
+		dots: true,
+		infinite: true,
+		speed: 500,
+		fade: true,
+		autoplay: true,
+		autoplaySpeed: 4000,
+		cssEase: 'linear'
+	});
+});
+$('#menu-btn').click(function(){		
+	$('.nav').slideToggle(700);	
+	if ($(this).hasClass('not-active')) {
+		$(this).addClass('is-active').removeClass('not-active');
+	}else{
+		setTimeout(function(){
+			$('.is-active').addClass('not-active').removeClass('is-active')
+		},600)			
+	}		
 });

e8330c01ba91f206b34d6bcf9c8c1a7cc3f01024
VirusBoy <iliya220499@gmail.com>
2017-05-09 18:42:47 +0600

    add contact-us

--- js/core.js
+++ js/core.js
@@ -41,3 +41,13 @@ $('#our-works').click(function(){
 		},600)			
 	}		
 });
+$('#contact-us').click(function(){		
+	$('.nav').slideToggle(700);	
+	if ($(this).hasClass('not-active')) {
+		$(this).addClass('is-active').removeClass('not-active');
+	}else{
+		setTimeout(function(){
+			$('.is-active').addClass('not-active').removeClass('is-active')
+		},600)			
+	}		
+});
